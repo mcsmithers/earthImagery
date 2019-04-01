5f78f0bf616a0a9cbccd6ed120bc1398bc7a985a
Christina Smithers <christina.smithers@gmail.com>
2019-04-01 15:04:21 -0600

    some light refactoring to be more vue canon

--- earth-imagery/client/src/components/PageHeader.vue
+++ earth-imagery/client/src/components/PageHeader.vue
@@ -7,9 +7,9 @@
       </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-            <v-btn flat dark v-if="!$store.state.isUserLoggedIn" v-on:click="navigateTo({name:'login'})">Login
+            <v-btn v-if="!$store.state.isUserLoggedIn" flat dark :to="{name: 'login'}">Login
             </v-btn>
-            <v-btn flat dark  v-if="!$store.state.isUserLoggedIn" v-on:click="navigateTo({name:'register'})">Sign Up
+            <v-btn flat dark  v-if="!$store.state.isUserLoggedIn" :to="{name: 'register'}">Sign Up
             </v-btn>
         </v-toolbar-items>
   </v-toolbar>
